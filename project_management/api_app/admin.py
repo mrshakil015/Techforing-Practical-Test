8db9bd743522b4c6362d705586257ad293402129
Md. Shakil <shakil.eub.cse@gmail.com>
2024-12-16 14:12:42 +0600

    Update admin.py

--- project_management/api_app/admin.py
+++ project_management/api_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Projects)
+admin.site.register(ProjectMember)
+admin.site.register(Tasks)
+admin.site.register(Comments)
